2fe248e28ba56c7ad4f87e3989fa3cbc3ba323a1
aayushbasnet <aayush.basnet@citytech.global>
2023-12-26 23:19:17 +0545

    feat: scanner in java

--- src/main/java/basics/dataTypes/NonPrimitiveDataType.java
+++ src/main/java/basics/dataTypes/NonPrimitiveDataType.java
@@ -5,9 +5,9 @@ public class NonPrimitiveDataType {
     public static void main(String[] args) {
         int intNumber = 9;
 
-        /*String is class (just a type). Here string is a type of object which can holds name.
+        /*String is class (just a type). Here String is a type of object which can holds name.
         And here name is a variable that is referring to an object of type String.
-        name doesnot contain or hold the object it just refers to the object of type String.*/
+        name does not contain or hold the object it just refers to the object of type String.*/
         String name = "Aayush Basnet";
         System.out.println("My name is: " + name);
     }
---
+++ src/main/java/basics/userInput/GettingUserInput.java
@@ -0,0 +1,36 @@
+package basics.userInput;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class GettingUserInput {
+    public static void main(String[] args) {
+        //Scanner class used to get input
+        Scanner userInput = new Scanner(System.in); // Create a Scanner object
+        boolean conversationStatus;
+        do {
+            try {
+                System.out.println("Enter your message: ");
+                String message = userInput.nextLine(); // reads Strings value from the user
+                System.out.println("You message is: " + message);
+
+                System.out.println("Are you a human?");
+                boolean isHuman = userInput.nextBoolean(); // reads boolean value from the user
+                String humanVerificationMessage = isHuman ? "You are a human" : "You are not human";
+                System.out.println(humanVerificationMessage);
+
+                System.out.println("How old are you?");
+                int userAge = userInput.nextInt();
+                System.out.println("So, you are " + userAge + " old. Hmm...");
+
+                System.out.println("What is your salary?");
+                float userSalary = userInput.nextFloat();
+                System.out.println("Oh! You earn " + userSalary);
+
+                conversationStatus = false;
+            } catch (InputMismatchException error) {
+                conversationStatus = true;
+            }
+        } while (conversationStatus);
+    }
+}
