44cf7fa39b58fbe3dd4a91e51efc54df0ee04dd0
aayushbasnet <aayush.basnet@citytech.global>
2023-12-26 21:44:38 +0545

    refactor: added few primitive data types

--- src/main/java/basics/Variable.java
+++ src/main/java/basics/Variable.java
@@ -8,6 +8,8 @@ public class Variable {
         float floatValue = 12.11111F;
         double doubleValue = 23.88687;
         char charValue = 'b';
+        boolean booleanValue = true; // true or false
+        byte byteValue = 127; // ranges from -128 to 127 (6 bit data)
 
         System.out.println("int " + intValue);
         System.out.println("short " + shortInt);
@@ -15,5 +17,7 @@ public class Variable {
         System.out.println("float " + floatValue);
         System.out.println("double " + doubleValue);
         System.out.println("char " + charValue);
+        System.out.println("boolean " + booleanValue);
+        System.out.println("byte " + byteValue);
     }
 }
