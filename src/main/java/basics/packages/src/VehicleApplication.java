5b3f3f091a0de1656574307c803c63f7fe133be8
aayushbasnet <aayushbasnet07@gmail.com>
2023-12-29 09:01:47 +0545

    feat: updated packages

--- src/main/java/basics/packages/src/VehicleApplication.java
+++ src/main/java/basics/packages/src/VehicleApplication.java
@@ -4,8 +4,21 @@ import basics.packages.vehicles.Car;
 public class VehicleApplication {
     public static void main(String[] args) {
         String model = "Tesla XYZ";
+        String color = "Purple";
+        int maxGear = 6;
+        float topSpeed = 167.89F;
+
         Car car = new Car();
         car.setModel(model);
-        System.out.println(car.getModel());
+        System.out.println("Model: " + car.getModel());
+
+        car.setColor(color);
+        System.out.println("Color: " + car.getColor());
+
+        car.setMaxGear(maxGear);
+        System.out.println("Maximum gear: " + car.getMaxGear());
+
+        car.setTopSpeed(topSpeed);
+        System.out.println("Top speed: " + car.getTopSpeed() + "km/hr");
     }
 }
